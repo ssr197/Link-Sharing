01e12a4d9c6b9f6fc13dd2120e9af252caa7b144
Saurabh Kumar <saurabh.kumar1@rxlogix.com>
2019-07-30 15:06:16 +0530

    Handled the exceptions of new signUp and login

--- grails-app/controllers/newlinksharingapp/DashboardController.groovy
+++ grails-app/controllers/newlinksharingapp/DashboardController.groovy
@@ -8,20 +8,34 @@ class DashboardController {
     def dashboard() {
         Users u1 = Users.findByEmail(session.name)
         Integer topicCount = dashboardService.totalTopicCount(session.name)
-        //println "The total number of topic are for user 1 = " + topicCount
         Integer subsCount = dashboardService.sCount(session.name)
-
         List  subscriptionLt = dashboardService.subscriptions(session.name)
+        if(!subscriptionLt)
+        {
+            List trending = dashboardService.trendtopics()
+            Users user = Users.findByEmail(session.name)
+            List topic1 = dashboardService.topTopicsPosts()
+            List subs1 = dashboardService.topTopicSubs()
+
+            render(view: "dashboard" ,model : [userdata : u1 ,
+                                               user: user,
+                                               count_topic : topicCount ,
+                                               count_subscribe  : subsCount ,
+                                               subscriptions : [],
+                                               subscount:[],
+                                               resourcecount:[] ,
+                                               trending : trending,
+                                               topic1 : topic1,
+                                               subs1: subs1]
+            )
+        }
+        else{
         List topicList = subscriptionLt*.topic
         List<Long> topicids = topicList.collect{it.id}
         List counts = dashboardService.subscriptioncount(topicids)
         List resourcecount = dashboardService.postscount(topicids)
         List trending = dashboardService.trendtopics()
-
-//        print "This is trending topics " + trending
         Users user = Users.findByEmail(session.name)
-
-
         List topic1 = dashboardService.topTopicsPosts()
         List subs1 = dashboardService.topTopicSubs()
 
@@ -47,7 +61,7 @@ class DashboardController {
         println "This is user resourcecount"+ resourcecount
         println "This is user trending "+ trending
 */
-    }
+    }}
 
 
     def forwardToUploadDocument(){
